d11c0aed56592242e1b780df604a26f06ec30d90
leen_odeh <123558998+Leen-odeh12@users.noreply.github.com>
2023-05-22 14:07:12 +0300

    Remove the extra lines and do the required

--- src/component/Links/Links.js
+++ src/component/Links/Links.js
@@ -1,11 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
 import Button from "@mui/material/Button";
-import MenuItem from "@mui/material/MenuItem";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 
 const pages = [
@@ -45,64 +40,6 @@ function Links() {
 
   return (
     <>
-      {isMobile && (
-        <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-          <IconButton
-            size="large"
-            aria-label="account of current user"
-            aria-controls="menu-appbar"
-            aria-haspopup="true"
-            onClick={handleOpenNavMenu}
-            color="black"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Menu
-            id="menu-appbar"
-            anchorEl={anchorElNav}
-            anchorOrigin={{
-              vertical: "bottom",
-              horizontal: "left",
-            }}
-            keepMounted
-            transformOrigin={{
-              vertical: "top",
-              horizontal: "left",
-            }}
-            open={Boolean(anchorElNav)}
-            onClose={handleCloseNavMenu}
-            sx={{
-              display: { xs: "block", md: "none", color: "black" },
-            }}
-          >
-            {pages.map((page) => (
-              <MenuItem
-                href={page}
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{
-                  width: "250px",
-                }}
-              >
-                <Typography
-                  textAlign="center"
-                  sx={{
-                    color: "#747579",
-                    textTransform: "uppercase",
-                    "&:hover": {
-                      fontWeight: "bold",
-                      color: "#FC6C46",
-                    },
-                  }}
-                >
-                  {page}
-                </Typography>
-              </MenuItem>
-            ))}
-          </Menu>
-        </Box>
-      )}
-
       <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
         {pages.map((page) => (
           <Button
